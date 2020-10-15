d7e840ee1dbb65cf12934f92da8e90089635738b
wlsdl5012 <62632099+jini5012@users.noreply.github.com>
2020-10-15 13:40:40 +0900

    Update build.gradle.kts

--- abilities/build.gradle.kts
+++ abilities/build.gradle.kts
@@ -29,7 +29,7 @@ subprojects {
         compileOnly(kotlin("reflect"))
         compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
         compileOnly("com.destroystokyo.paper:paper-api:1.16.3-R0.1-SNAPSHOT")
-        compileOnly("com.github.noonmaru:tap:3.2.0")
+        compileOnly("com.github.noonmaru:tap:3.2.3")
         compileOnly("com.github.noonmaru:psychics:0.8.0")
 
 //        testImplementation("junit:junit:4.13")
